dd5c51ebbb8bf3252950e86930fd2f116fa5d6f1
suresh <suresh.appadurai@dreamguystech.com>
2023-09-29 18:32:40 +0530

    header added

---
+++ src/pages/header/header.tsx
@@ -0,0 +1,13 @@
+import { HeaderOne } from "./headerOne";
+import { HeaderTwo } from "./headerTwo";
+
+const Header = () => {
+  return (
+    <div>
+      <HeaderOne />
+      <HeaderTwo />
+    </div>
+  );
+};
+
+export default Header;
--- src/routes/path.tsx
+++ src/routes/path.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { dashboardUrl } from "../assets/constants/pageurl";
 import Dashboard from "../pages/dashboard";
+import HeaderLayout from "../pages/header/headerlayout";
 
 export const RouteObject = [
   {
